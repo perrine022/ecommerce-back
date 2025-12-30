d11cd6c42760fe6370f9b1b324f99409206f890d
perrine022 <perrine.honore@gmail.com>
2025-12-30 19:28:39 +0100

    Update schema.sql: make campaign field NOT NULL and set composite primary keys for user_marketing_campaigns and contact_sellsy_marketing_campaigns tables

--- src/main/java/com/tradeall/tradefood/config/SecurityConfig.java
+++ src/main/java/com/tradeall/tradefood/config/SecurityConfig.java
@@ -53,9 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
